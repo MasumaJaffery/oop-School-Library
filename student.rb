dbe3736c190c07aefd961b96600966ec5392b39f
MasumaJaffery <masumajaffery1@gmail.com>
2023-09-13 06:26:49 -0700

    try to fix

--- student.rb
+++ student.rb
@@ -1,11 +1,13 @@
 require_relative 'person';
-# We implement Ineritance Concept here that's about child class inherits 
+# We implement Ineritance Concept here that's about subclass inherits superclass (methods and properties)
 class Student < Person
 # Accessors
 attr_accessor :classroom
+# Initialize Method
 def initialize(name = "unknown", age, parent_permission:true, classroom)
+# super keyword is used to call initialize method of Parent Class.
 super(name,age,parent_permission)
-@classroom : classroom
+@classroom = classroom
 end
 def play_hooky
     ¯\(ツ)/¯
