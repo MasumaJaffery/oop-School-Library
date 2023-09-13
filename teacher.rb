b2ad5a6cfd563d3d9f4768e9783587995635f4af
MasumaJaffery <masumajaffery1@gmail.com>
2023-09-13 11:54:59 -0700

    Fix

--- teacher.rb
+++ teacher.rb
@@ -1,15 +1,17 @@
-require_relative 'person';
+require_relative 'person'
 # We implement Ineritance Concept here that's about subclass inherits superclass (methods and properties)
 class Teacher < Person
-# Accessors
-attr_accessor :specialization
-# Initialize Method
-def initialize(name = "unknown", age, parent_permission:true, specialization)
-# super keyword is used to call initialize method of Parent Class.
-super(name,age,parent_permission)
-@specialization = specialization
-end
-def can_use_services?
+  # Accessors
+  attr_accessor :specialization
+
+  # Initialize Method
+  def initialize(specialization, age, name = 'unknown', parent_permission: true)
+    # super keyword is used to call initialize method of Parent Class.
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
     true
-end
+  end
 end
