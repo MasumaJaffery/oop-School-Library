51378759fa4c5ea3bf25bc17aa4b5dc9296f3cf7
MasumaJaffery <masumajaffery1@gmail.com>
2023-09-14 05:06:38 -0700

    Fix

--- nameable.rb
+++ nameable.rb
@@ -19,13 +19,12 @@ end
 
 class CapitalizeDecorator < BaseDecorator
   def correct_name
-    super().upcase
+    @nameable.correct_name.capitalize
   end
 end
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    name = super()
-    name.length > 10 ? name[0...10] : name
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
